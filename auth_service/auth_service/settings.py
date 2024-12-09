f92ec5ab29a3bdd96afa209c945a58c853333d91
bazabizi <bazabizi3@gmail.com>
2024-12-09 12:54:26 +0300

    change allowed hosts

--- auth_service/auth_service/settings.py
+++ auth_service/auth_service/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # Application definition
 
