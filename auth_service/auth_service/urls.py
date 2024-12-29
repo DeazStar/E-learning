08d1ab9526fbe867e22ea6a9c4da858812fa003b
mahlet13 <122526725+mahlet13@users.noreply.github.com>
2024-12-29 16:34:13 +0300

    Update urls.py

--- auth_service/auth_service/urls.py
+++ auth_service/auth_service/urls.py
@@ -1,14 +1,23 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CustomTokenObtainPairView, RegisterView, RoleBasedView , RequestResetPasswordView , VerifyOtpAndResetPasswordView,VerifyTokenView
+"""
+URL configuration for auth_service project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('get_me/', RoleBasedView.as_view(), name='role_based'),
-    path('password-reset/request/', RequestResetPasswordView.as_view(), name='password_reset_request'),
-    path('password-reset/verify/', VerifyOtpAndResetPasswordView.as_view(), name='password_reset_verify'),
-    path('verify-token/', VerifyTokenView.as_view(), name='verify-token'),
-
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
 ]
