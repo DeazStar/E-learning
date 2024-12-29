e724f2344b4c2e36beb2fc67cbf5777ec4ed9303
mahlet13 <122526725+mahlet13@users.noreply.github.com>
2024-12-29 16:34:47 +0300

    Add files via upload

--- auth_service/users/urls.py
+++ auth_service/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, RoleBasedView , RequestResetPasswordView , VerifyOtpAndResetPasswordView
+from .views import CustomTokenObtainPairView, RegisterView, RoleBasedView , RequestResetPasswordView , VerifyOtpAndResetPasswordView,VerifyTokenView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('get_me/', RoleBasedView.as_view(), name='role_based'),
     path('password-reset/request/', RequestResetPasswordView.as_view(), name='password_reset_request'),
     path('password-reset/verify/', VerifyOtpAndResetPasswordView.as_view(), name='password_reset_verify'),
+    path('verify-token/', VerifyTokenView.as_view(), name='verify-token'),
+
 ]
